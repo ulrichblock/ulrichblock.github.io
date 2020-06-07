87a06a6de15e4e40b26ef788f525b8c5004386c6
Ulrich Block <u.block@sap.com>
2020-06-07 14:33:38 +0200

    configure rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,7 +149,61 @@ module.exports = {
       }
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-feed',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                const url = `${site.siteMetadata.siteUrl}${edge.node.fields.slug}`
+
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: url,
+                  guid: url,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: 'https://www.ulrich-block.de RSS Feed'
+          }
+        ]
+      }
+    },
     {
       resolve: 'gatsby-plugin-sitemap',
       options: {
